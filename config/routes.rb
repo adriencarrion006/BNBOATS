55412cc0543857ceb906756e1e37a4f8f1f30825
Adrien CARRION <adriencarrion006@gmail.com>
2022-06-07 17:51:46 +0200

    view rents

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :boats 
+  resources :boats do
+    resources :rents, only: [:new, :create]
+  end
 
+
+resources :rents, only: [:destroy, :update, :edit, :index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
